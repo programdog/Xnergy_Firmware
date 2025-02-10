dbe556bb0fa6aa9c584f1effbd82c3e772404ebd
renzhongyu <531353008@qq.com>
2025-02-10 17:25:40 +0800

    1.add comment.

--- data_structure.h
+++ data_structure.h
@@ -4,20 +4,20 @@
 //MCU A robot 
 typedef struct ROBOT
 {
-	uint8_t id;
-	uint8_t battery_percentage;
-	uint8_t charging_status;
-	uint8_t charger_id;
+	uint8_t id; //robot id
+	uint8_t battery_percentage; //battery percentage
+	uint8_t charging_status; //robot charging status
+	uint8_t charger_id; //
 } ROBOT;
 
 
 //MCB B
 typedef struct CHARGER
 {
-	uint8_t id;
-	uint8_t battery_percentage;
+	uint8_t id; // charger id
+	uint8_t battery_percentage; //robot battery percentage
 	uint8_t charging_status; //free or not
-	uint8_t robot_id;
+	uint8_t robot_id; 
 } CHARGER;
 
 //hand shake
